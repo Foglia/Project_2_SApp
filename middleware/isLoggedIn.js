704beed7c383550f70ae4ab965e5a2570c6e093f
Flavia Fogliato <flavia.fogliato@gmail.com>
2022-11-07 11:45:18 +0000

    no more errors

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-const User = require("../models/User.model"); 
-
->>>>>>> 1039c8cd3ff463367b307d83a78ed15c6b41c658
 module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.currentUser) {
@@ -11,9 +6,3 @@ module.exports = (req, res, next) => {
 
   next();
 };
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 1039c8cd3ff463367b307d83a78ed15c6b41c658
