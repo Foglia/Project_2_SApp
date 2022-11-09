adafa711b7330d09f459ae36fe53d6290a272a6e
Flavia Fogliato <flavia.fogliato@gmail.com>
2022-11-09 17:08:51 +0000

    nav bar functions

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User.model");
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  const currentUser = req.session.currentUser
+  if(!currentUser) {
+    res.render("index");
+  } else {
+    const currentUser = req.session.currentUser
+    const user = User.find()
+    const artist = req.session.currentUser.accountType === "Artist"
+    res.render("index", {currentUser, artist, user});
+  }
 });
 
 module.exports = router;
